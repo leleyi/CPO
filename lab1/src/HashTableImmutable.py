de1c9cdd650ca85fc7e3c2da9087f1b2b4c11268
leleyi <653574281@qq.com>
2020-05-06 16:29:13 +0800

    update immutable

--- lab1/src/HashTableImmutable.py
+++ lab1/src/HashTableImmutable.py
@@ -1,5 +1,4 @@
-from HashMap import *
-import copy
+from lab1.src.HashMap import *
 
 
 class HashTableImmutable(HashMap):
@@ -8,27 +7,26 @@ class HashTableImmutable(HashMap):
         self._len = 0
         self.kvEntry = [self._empty] * size
         self.index = 0
-
+        if kwds.__len__() != 0:
+            hashmap = HashMap(**kwds)
         if hashmap is not None:
             for entry in hashmap.kvEntry:
                 if entry is hashmap._empty or entry is hashmap._deleted:
                     continue
                 else:
+                    print("do this?")
                     super(HashTableImmutable, self).put(entry.key, entry.value)
-        elif kwds.__len__() != 0:
-            print(kwds)
-            super(HashTableImmutable, self).__init__(**kwds)
 
     def put(self, key, value):
         print("It's a immutable version put.It will return a new HashTableImmutable.")
 
         table = HashMap()
+        table.put(key, value)
         for entry in self.kvEntry:
             if entry is self._empty or entry is self._deleted:
                 continue
             else:
                 table.put(entry.key, entry.value)
-        table.put(key, value)
 
         return HashTableImmutable(table)
 
