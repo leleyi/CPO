b64a75947ae4419a0b69feac5dde8621ebc91e5b
gulico <767293861@qq.com>
2020-05-20 10:03:51 +0800

    add type annotations.

--- lab1/src/immutable.py
+++ lab1/src/immutable.py
@@ -2,22 +2,37 @@ from mutable import *
 
 
 def cons(map):
+    """
+    Copy a hash map
+
+    :param map: HashMap
+    :return: The copied hash map
+    """
     table = HashMap()
     table.from_dict(map.to_dict())
     return table
 
 
 def size(map):
+    """
+    Get the size of the hash map
+
+    :param map: HashMap
+    :return: the size of the hash map
+    """
     if map is None:
         return 0
     else:
         return len(map)
 
 
-"""the order is not change"""
-
-
 def to_dict(map):
+    """
+    Convert hash map to dictionary
+
+    :param map: HashMap
+    :return: dict
+    """
     kvlist = {}
     if map is None:
         return kvlist
@@ -30,6 +45,12 @@ def to_dict(map):
 
 
 def to_list(map):
+    """
+    Convert hash map to list
+
+    :param map: HashMap
+    :return: list
+    """
     res = []
     if map is None:
         return res
@@ -45,6 +66,12 @@ def to_list(map):
 #     return table
 
 def from_list(list):
+    """
+    Convert list to hash map
+
+    :param list: list
+    :return: The converted hash map
+    """
     table = HashMap()
     for i, v in enumerate(list):
         table.put(i, v)
@@ -52,28 +79,64 @@ def from_list(list):
 
 
 def put(map, key, value):
+    """
+    Insert key-value pairs into hash map
+
+    :param map: HashMap
+    :param key: The key value to insert into the hash map
+    :param value: The content corresponding to the key value of the hash map to be inserted
+    :return: Hash map obtained after inserting key-value pairs
+    """
     table = cons(map)
     table.put(key, value)
     return table
 
 
 def get(map, key):
+    """
+    Get the corresponding content from the hash map according to the specified key value
+
+    :param map: HashMap
+    :param key: key
+    :return: value
+    """
     return map.get(key)
 
 
 def put_dic(map, **kwargs):
+    """
+    Convert dict to hash map
+
+    :param map: HashMap
+    :param kwargs: A string of key-value pairs
+    :return: HashMap
+    """
     table = cons(map)
     table.put_dic(**kwargs)
     return table
 
 
 def del_(map, key):
+    """
+    Delete the value of the given key from the hash map
+
+    :param map: HashMap
+    :param key: key
+    :return: HashMap
+    """
     table = cons(map)
     table.del_(key)
     return table
 
 
 def mconcat(map1, map2):
+    """
+    concat two maps to one
+
+    :param map1: HashMap
+    :param map2: HashMap
+    :return: HashMap
+    """
     if map1 is None and map2 is None:
         return None
     if map1 is None:
@@ -86,6 +149,13 @@ def mconcat(map1, map2):
 
 
 def map(map, f):
+    """
+    map the map element to the f
+
+    :param map1: HashMap
+    :param f: the function to map
+    :return: HashMap
+    """
     table = cons(map)
     for key in map._keyset:
         value = map.get(key)
@@ -95,6 +165,14 @@ def map(map, f):
 
 
 def reduce(map, f, initial_state):
+    """
+    reduce the mapSet to one value
+
+    :param map: HashMap
+    :param f: the reduce method
+    :param initial_state: result initial_state
+    :return: final result
+    """
     state = initial_state
     for key in map._keyset:
         value = map.get(key)
@@ -103,6 +181,13 @@ def reduce(map, f, initial_state):
 
 
 def get_hash(map, key):
+    """
+    get the hash_value that had saved the map
+
+    :param map: HashMap
+    :param key:
+    :return: hash_value
+    """
     return map.get_hash(key)
 
 
